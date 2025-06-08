106b50fd10d5ac2b563dad45b18615bbe38e1d91
Mohammedriad12 <156068664+Mohammedriad12@users.noreply.github.com>
2025-06-08 12:33:17 +0300

    Time: 0 ms (100%), Space: 9.2 MB (29.41%) - LeetHub

--- 0728-self-dividing-numbers/0728-self-dividing-numbers.cpp
+++ 0728-self-dividing-numbers/0728-self-dividing-numbers.cpp
@@ -2,18 +2,21 @@ class Solution {
 public:
     vector<int> selfDividingNumbers(int left, int right) {
         vector<int>result;
-        for (int i = left; i <= right; i++){
-            int n = i;
-            bool x = true;
-            while(n){
-                int d = n % 10;
-                if(d == 0 || i % d){
-                x = false;
+
+        for(int i = left; i <= right; i++){
+           int n = i;
+           bool dividable = true;
+
+           while(n){
+            int digit = n % 10;
+            
+            if(digit == 0 || i % digit){
+                dividable = false;
                 break;
             }
-                n /= 10; 
-            }
-            if (x) result.push_back(i);
+            n /= 10;
+           } 
+           if (dividable) result.push_back(i);
         }
         return result;
     }
